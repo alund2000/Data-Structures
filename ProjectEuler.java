e48969acf38f8c0a9927f3749fc34e62cb257a76
Abigail Lund <abigailrlund@gmail.com>
2018-09-18 09:07:14 -0400

    problem 1 solution

--- ProjectEuler.java
+++ ProjectEuler.java
@@ -1,11 +1,18 @@
 public class ProjectEuler{
 	public static void main(String[] args){
+		System.out.println(sumMultiples(1000));
 
  }
 
 
  //Problem 1: find the sum of all the multiples of 3 or 5 below a given #
-	public int sumMultiples(int ceiling){
-	
+	public static String sumMultiples(int ceiling){
+		int sum=0;
+		for(int i=0;i<ceiling;i++){
+			if(i%3==0||i%5==0){
+				sum+=i;
+			}
+		}
+		return("The sum of the multiples of 5 & 3 from 0 to "+ceiling+" is "+ sum);
 	}
 }
