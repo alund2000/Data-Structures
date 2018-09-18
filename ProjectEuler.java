6411af1fbec403dc3820eb19d305dffce3371d02
Abigail Lund <abigailrlund@gmail.com>
2018-09-18 08:56:20 -0400

    Problem 1 begin

--- ProjectEuler.java
+++ ProjectEuler.java
@@ -2,4 +2,10 @@ public class ProjectEuler{
 	public static void main(String[] args){
 
  }
+
+
+ //Problem 1: find the sum of all the multiples of 3 or 5 below a given #
+	public int sumMultiples(int ceiling){
+	
+	}
 }
