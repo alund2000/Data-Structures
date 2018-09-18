143188fa3ee3114568e55f142b45b3ecfd343233
Abigail Lund <abigailrlund@gmail.com>
2018-09-18 09:31:10 -0400

    Problem 2 begin

--- ProjectEulerP2.java
+++ ProjectEulerP2.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
 public class ProjectEulerP2{
 	public static void main(String[] args){
 
  }
+
+ //Problem 2: gives the sum of the even Fibbonacci Values
+ 	//REMINDER: GO BACK AND MAKE MORE EFFICIENT BY MAKING CODE RECURSIVE
+ 	public static String sumEvenFib(int ceiling){
+ 		int sum=0;
+ 		ArrayList<Integer> fibNums= new ArrayList<Integer>();
+ 		fibNums.add(1);
+ 		fibNums.add(2);
+ 		//fill fibNums with fibonacci numbers up to ceiling
+ 		return "";
+ 		
+ 	}
+
 }
